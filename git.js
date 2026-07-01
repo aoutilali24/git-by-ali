6e58f71616189080ad762296b0c62d6d2e8b182e
aoutilali24 <aoutilali24@gmail.com>
2026-07-01 16:55:31 +0100

    created a git function/repo/commit

--- git.js
+++ git.js
@@ -1,6 +1,21 @@
-console.log("hello deutschland");
-
-function Git(){
- console.log("first line fr")
-
+function Git(name){
+ this.name= name;
+   this.lastCommitId = -1;
 }
+
+var repo = new Git("new-repo")
+/**Actual command is > git init */
+
+function Commit(id, message){
+    this.id= id;
+    //this is assuming that "this" has a "change" property too since implementing is beyond the scope of this simple recreation for now
+    this.message= message;
+}
+
+Git.prototype.commit = function(message){
+    var commit = new Commit(++this.lastCommitId, message);
+    return commit;
+}
+
+repo.commit("is Commit working ?")
+console.log(repo.commit("is Commit working?"));
